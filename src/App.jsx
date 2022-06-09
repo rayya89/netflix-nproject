04425e5e9f1911a7874ecbb15b7483ec7d0d10b7
Rayya <rayyatar@gmail.com>
2022-06-09 15:55:09 +0200

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { Routes, Route } from "react-router-dom";
 import "./styles/styles.css";
 import Signup from "./pages/Signup";
 import Registration from "./pages/Registration";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="signup" element={<Signup />} />
           <Route path="/signup/regform" element={<Registration />} />
         </Routes>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,59 @@
+// NPM packages
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+//Program files
+import logo from "../assets/pictures/logo.png";
+import InputField from "../components/InputField";
+import form from "../data/loginForm.json";
+import { loginUser } from "../scripts/firebaseAuth";
+import { getDocument } from "../scripts/fireStore";
+import { useUser } from "../state/UserContext";
+import { onFail } from "../scripts/onFail";
+
+export default function Login() {
+  //Global state
+  const { setUser } = useUser();
+
+  // Local state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //Properties
+  const navigate = useNavigate();
+
+  //Methods
+  async function onLogin(event) {
+    event.preventDefault();
+    let user;
+    const uid = await loginUser(email, password).catch(onFail);
+    if (uid) user = await getDocument("users", uid).catch(onFail);
+
+    if (user) onSuccess(user);
+  }
+
+  function onSuccess(user) {
+    setUser(user);
+    user.role === "member" && navigate("/dashboard");
+    user.role === "admin" && navigate("/admin");
+  }
+
+  return (
+    <div>
+      <header>
+        <img className="logo" src={logo} alt="Netflix" />
+      </header>
+      <div>
+        <h1>Sign In</h1>
+        <form className="form" onSubmit={onLogin}>
+          <InputField setup={form.email} state={[email, setEmail]} />
+          <InputField setup={form.password} state={[password, setPassword]} />
+          <button>Sign in</button>
+        </form>
+        <Link to="/LoginHelp">Need help?</Link>
+        <p>New to Netflix?</p>
+        <Link to="/signup">Sign up now</Link>
+      </div>
+    </div>
+  );
+}
